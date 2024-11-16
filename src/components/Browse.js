5a8d6c9bbfe253c828615524410c2bf2253cb664
KolluriVenkataSravani <sravanikv2003@gmail.com>
2024-11-16 14:39:43 +0530

    Build Video Title in Main Container

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,23 @@
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/*
+          MainContainer
+            - Video Background
+            - Video Title
+          SecondaryContainer
+            - MovieList * n
+              - cards * n
+        */}
     </div>
   )
 }
